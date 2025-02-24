4928e6f412c9a619ed4b3f80795993e3876d9201
nishay <nishatayub09@icloud.com>
2025-02-24 15:15:25 +0530

    project yet to be deployed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import TeamMemberCard from './components/TeamMemberCard.jsx'
 import './App.css'
 
@@ -8,6 +7,7 @@ function App() {
   return (
     <>
       <TeamMemberCard name="John Doe" title="Software Engineer"/>
+      <TeamMemberCard name="Nishat Ayub" title="Software Developer"/>
     </>
   )
 }
